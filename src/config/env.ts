fcd3ae13b4ef980849eb16991a91689a22bb5a1d
Edilson Luna <edilsonluna117@gmail.com>
2026-01-10 22:12:22 -0500

    Ajustes para conexion con openai

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,13 +3,6 @@ import 'dotenv/config';
 export const env = {
     NODE_ENV : process.env.NODE_ENV ?? 'development',
     PORT : process.env.PORT ?? 3000,
-    DB_HOST : process.env.DB_HOST ?? 'localhost',
-    DB_USER : process.env.DB_USER ?? 'root',
-    DB_PASS : process.env.DB_PASS ?? '',
-    DB_NAME : process.env.DB_NAME ?? 'empenosysdb',
-    DB_PORT : process.env.DB_PORT ?? 3306,
-    FIRMA_ACCESS_TOKEN : process.env.FIRMA_ACCESS_TOKEN ?? '',
-    ACCESS_TOKEN_DURATION : process.env.ACCESS_TOKEN_DURATION ?? '15Min',
-    FIRMA_REFRESH_TOKEN : process.env.FIRMA_REFRESH_TOKEN ?? '',
-    REFRESH_TOKEN_DURATION : process.env.REFRESH_TOKEN_DURATION ?? '7Days',
+    VERIFY_TOKEN : process.env.VERIFY_TOKEN || '',
+    OPENAI_API_KEY : process.env.OPENAI_API_KEY || ''
 }
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,7 +1,11 @@
 import { Request, Response, Router } from "express";
-// Token que vas a usar para la verificación
-const VERIFY_TOKEN = 'prymusoft_test_token';
+import OpenAI from "openai";
+import { env } from "../config/env";
 
+
+const client = new OpenAI({
+    apiKey: env.OPENAI_API_KEY
+});
 const router = Router();
 
 // Webhook: verificación GET
@@ -12,7 +16,7 @@ router.get('/webhook', (req:Request, res:Response) => {
     const challenge = req.query['hub.challenge'];
 
     if (mode && token) {
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    if (mode === 'subscribe' && token === env.VERIFY_TOKEN) {
         console.log('Webhook verificado');
         return res.status(200).send(challenge); // Muy importante: devuelve el challenge
     } else {
@@ -27,8 +31,29 @@ router.post('/webhook', (req:Request, res:Response) => {
     console.log('peticion agregada correctamente!!!');
     console.log("cuerpo de peticion");
     console.log(req.body);
+    console.log(JSON.stringify(req.body.entry[0].changes,null,2));
+
     return res.status(200).json({"message": "hello world"});
 });
 
+router.post('/pruebaOpenAI', async (req:Request, res:Response) => {
+    const respuesta = await responderAI();
+    return res.status(200).json({"message": respuesta});
+});
+
+
+async function responderAI() {
+  const completion = await client.responses.create({
+    model: "gpt-4o-mini",
+    // reasoning: {effort: 'low'},
+    input: [
+      { role: "developer", content: "Eres un asistente de ventas de celulares. Debes tener un tono amable, calido, que haga sentir en confianza al comprador. A su vez, debes usar palabras que enganchen y emocionen a la persona a comprar los productos. El stock de la tienda es iphone 14 pro de 128 gb color negro. Con valor de 2'500'000 COP. Acercandose antes del 12/01/2025 puede tener 20% de descuento." },
+      { role: "user", content: "Hola. Quisiera informacion sobre los telefonos" }
+    ]
+  });
+  console.log('respuesta de openai');
+  console.log(JSON.stringify(completion));
+  return completion.output_text;
+}
 
 export default router;
