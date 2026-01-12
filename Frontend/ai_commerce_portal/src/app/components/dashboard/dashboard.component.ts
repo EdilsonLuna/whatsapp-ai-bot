c21c4560971c5ccad4bce9fc1e93b34b7cc9c27f
Edilson Luna <edilsonluna117@gmail.com>
2026-01-11 22:24:54 -0500

    Ajustes para side-bar

--- Frontend/ai_commerce_portal/src/app/components/dashboard/dashboard.component.ts
+++ Frontend/ai_commerce_portal/src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
-    CommonModule
-  ],
+    CommonModule,
+    RouterModule
+],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
