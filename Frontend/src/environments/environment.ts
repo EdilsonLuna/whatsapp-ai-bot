3e8b111e499faac8769146549d7a5688b94f608c
Edilson Luna <edilsonluna117@gmail.com>
2026-04-02 19:37:41 -0500

    ajustes para login de pagina

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const env = {
-    API_URL : "http://localhost:3800/api",
+    API_URL : "http://localhost:4000/api",
     NOMBRE_EMPRESA : "Prymusoft",
     EMBLEMA_EMPRESA : "Sistema de Gestión y Administración de Atención al cliente con IA"
 }
